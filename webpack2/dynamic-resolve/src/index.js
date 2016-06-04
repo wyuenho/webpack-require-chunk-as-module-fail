41e24e38e6445ff5df08b36e876ba2e934859199
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2016-06-04 16:56:44 +0100

    Replace fetch with promisescript

--- webpack2/dynamic-resolve/src/index.js
+++ webpack2/dynamic-resolve/src/index.js
@@ -1,3 +1,4 @@
+var promisescript = require('promisescript');
 var a = './a';
 
 // console.log(require.resolve(a));
@@ -5,7 +6,7 @@ var a = './a';
 // As long as module `a` is set as an entry point, Webpack refuses to generate a
 // mapping from path to internal module ID (AKA context in Webpack parlance) for
 // module `a` in the bundle.
-fetch(__webpack_public_path__ + 'a.js').then(function () {
+promisescript(__webpack_public_path__ + 'a.js').then(function () {
   // Uncommenting this line will result in a build error
   // console.log(require.resolve('./a'));
 
