be98a8296d01895635e929d2d00b307b6deaf53d
Jimmy Yuen Ho Wong <wyuenho@users.noreply.github.com>
2016-06-04 02:21:09 +0100

    Update webpack.bad.1.config.js

--- webpack2/dynamic-resolve/webpack.bad.1.config.js
+++ webpack2/dynamic-resolve/webpack.bad.1.config.js
@@ -5,9 +5,9 @@ module.exports = {
 
   debug: true,
 
-  // Webpack 2, as of 2.1.0-beta.8, has fixed the bug of unnecessary requiring
+  // Webpack 2, as of 2.1.0-beta.8, has fixed the bug of unnecessarily requiring
   // entry point warning, but lost the ability to chase down references, this
-  // good base in Webpack 1 now fails too.
+  // good case in Webpack 1 now fails too.
   entry: {
     index: "./src/index.js",
   },
